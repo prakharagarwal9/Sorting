b05bc2391dce7627036bf259d9676b664a9036bb
Prakhar Agarwal <Prakhar Agarwal@10.12.10.153>
2022-09-22 09:28:26 +0530

    newsort

---
+++ src/lab_3/newsort.java
@@ -0,0 +1,34 @@
+package lab_3;
+
+public class newsort {
+	
+	public static void newsort(int array[]) {  
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key;  
+        }  
+    }  
+	
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		newsort ob = new newsort();
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+		ob.newsort(arr);
+		ob.printArray(arr);
+
+	}
+
+}
